511bb7d260b963a6139eaee887efb87a4387746b
Jeremy Martinez <42855827+Mozz96@users.noreply.github.com>
2026-02-19 20:37:36 -0600

    v1
    completed assignment

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ let poemStanza = ["Apples are Red,", "The sky is Blue,", "On this day of coding,
 
 console.log("A Poem Through Coding:");
 
+let i = 106;
+let text = ""
+
+
 for (poem of poemStanza) {   
     console.log(poem);
 
@@ -9,9 +13,19 @@ for (poem of poemStanza) {
         console.log("Well well well, I'm a poet and I didn't even know it! \(Ok ok, I'll just stick to the coding haha!\)")
         break;
         
-    } else {
-        console.log("(Not at the end just yet!)")
-        }
+    } else{
+        console.log("Not at the end just yet! \(Not that you'd want it to be done of course, right?\)")
+    }
 }
 
+console.log("Now, watch me go from 106 to 96!");
+
+while (i > 96){
+    i--;
+    
+    if(i = 96){
+        console.log("The number is now 96, like 1996 my birth year!")
+        break;
+    }
+}
 
